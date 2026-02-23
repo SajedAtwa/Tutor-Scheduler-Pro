49a9a9ff2415ae165cc5048db085e948aa02332c
SajedAtwa <sajedatwa78@gmail.com>
2026-02-22 21:06:22 -0500

    bug fix

--- server/routes/providerRoutes.js
+++ server/routes/providerRoutes.js
@@ -11,12 +11,12 @@ router.post('/loginP', providerController.loginProvider);
 // Apply authMiddleware to all subsequent routes
 router.use(authMiddleware);
 
-router.get('/addresses', roleMiddleware(['provider']), providerController.listAddresses);
-router.get('/classes/:serviceType', roleMiddleware(['provider']), providerController.selectClassType);
-router.post('/address', roleMiddleware(['provider']), providerController.enterAddressDetails);
-router.post('/create', roleMiddleware(['provider']), providerController.createClass);
-router.get('/list', roleMiddleware(['provider']), providerController.listClasses);
-router.put('/update', roleMiddleware(['provider']), providerController.updateClass);
-router.delete('/remove', roleMiddleware(['provider']), providerController.removeClass);
+router.get('/addresses', roleMiddleware('provider'), providerController.listAddresses);
+router.get('/classes/:serviceType', roleMiddleware('provider'), providerController.selectClassType);
+router.post('/address', roleMiddleware('provider'), providerController.enterAddressDetails);
+router.post('/create', roleMiddleware('provider'), providerController.createClass);
+router.get('/list', roleMiddleware('provider'), providerController.listClasses);
+router.put('/update', roleMiddleware('provider'), providerController.updateClass);
+router.delete('/remove', roleMiddleware('provider'), providerController.removeClass);
 
 module.exports = router;
